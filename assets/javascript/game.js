dfa91d4acb08fbd474202b3802283267b3389a5d
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2018-04-01 11:56:58 -0400

    Added motto hint and flag hint functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,8 @@ var randSt = "";
 var winningNumber = 0;
 var lost = false;
 var won = false;
+var randStMotto = "";
+var stFlag = "";
 
 function resetGame() {
   disableButton();
@@ -9,6 +11,7 @@ function resetGame() {
   randSt = randSelect.name;
   winningNumber = randSt.length;
   randStMotto = randSelect.stateMoto;
+  stFlag = "./assets/images/"+randSelect.flag;
   console.log(randSt, randStMotto);
 
   var buf = "";
@@ -20,10 +23,23 @@ function resetGame() {
   document.getElementById("dashes").textContent = buf;
   document.getElementById("guessed").innerHTML = '&nbsp;';
   document.getElementById("counter").textContent = 10;
-  document.getElementById("hintAboutState").textContent = randStMotto;
+  document.getElementById("hintAboutState").innerHTML = '&nbsp;';
+  document.getElementById("flagHint").setAttribute("src","");
+  document.getElementById("flagHint").setAttribute("alt","");
+
+
   found = false;
   lost = false;
   won = false;
+}
+
+function showMotto() {
+  document.getElementById("hintAboutState").textContent = randStMotto;
+}
+
+function showFlag() {
+  document.getElementById("flagHint").setAttribute("src",stFlag);
+  document.getElementById("flagHint").setAttribute("alt",randSt);
 }
 
 function init() {
@@ -43,9 +59,21 @@ function init() {
   dashes.textContent = buf;
   dashes.setAttribute("id", "dashes");
   document.getElementById("play-area").appendChild(dashes);
-  document.getElementById("hintAboutState").textContent = randStMotto;
+  // document.getElementById("hintAboutState").textContent = randStMotto;
+  stFlag = "./assets/images/"+randSelect.flag;
+
   document.getElementById("newGameBtn").addEventListener("click", function() {
     resetGame();
+    // showMotto();//reset the motto
+  });
+
+  document.getElementById("mottoBtn").addEventListener("click", function() {
+    showMotto();
+  });
+
+  document.getElementById("flagBtn").addEventListener("click", function() {
+    // showMotto();
+    showFlag();
   });
 }
 
@@ -66,8 +94,9 @@ window.addEventListener("keypress", function(keypress) {
       alert("You have won already; please start a new game!");
     } else if (lost) {
       alert("You have lost; please start a new game!");
+      enableButton();
+      document.getElementById("dashes").textContent = randSt;
     } else {
-      console.log("\"" + keypress + "\"");
       // and if the game has not been lost go on to add to the already guessed-so-far list
       document.getElementById("guessed").textContent += keypress.key + " ";
 
@@ -91,15 +120,21 @@ window.addEventListener("keypress", function(keypress) {
           number--;
           console.log(" chance remain number derecremented " + number);
           wrongScore.innerHTML = number;
+          console.log(number == 0);
           if (number == 0) {
             //lost the game in the last key press
             lost = true;
-            this.document.getElementById("dashes").textContent = randSt;
-
+            console.log(document.getElementById("dashes").textContent);
+            console.log(randSt);
+            document.getElementById("dashes").textContent = randSt;
+            enableButton();
             // enable button
             // show the answer
           }
         }
+      }
+      else {
+        document.getElementById("dashes").textContent = dashText.join(" ");
       }
 
       if (winningNumber == 0 && !won) {
@@ -111,30 +146,32 @@ window.addEventListener("keypress", function(keypress) {
         enableButton();
       }
 
-      document.getElementById("dashes").textContent = dashText.join(" ");
     }
   } else {
-    console.log("pressed key is new");
+    console.log("pressed key is not new");
     if (lost) {
       alert("You have lost; please start a new game!");
+      document.getElementById("dashes").textContent = randSt;
+      enableButton();
     } else if (won) {
       alert("You have won already; please start a new game!");
     } else {
-      var wrongScore = document.getElementById("counter");
-      var number = wrongScore.innerHTML;
-      if (number > 0) {
-        number--;
-        console.log(" chance remain number derecremented " + number);
-        wrongScore.innerHTML = number;
-        if (number == 0) {
-          //lost the game in the last key press
-          lost = true;
-          document.getElementById("dashes").textContent = randSt;
-          enableButton();
-          // enable button
-          // show the answer
-        }
-      }
+
+      // var wrongScore = document.getElementById("counter");
+      // var number = wrongScore.innerHTML;
+      // if (number > 0) {
+      //   number--;
+      //   console.log(" chance remain number derecremented " + number);
+      //   wrongScore.innerHTML = number;
+      //   if (number == 0) {
+      //     //lost the game in the last key press
+      //     lost = true;
+      //     document.getElementById("dashes").textContent = randSt;
+      //     enableButton();
+      //     // enable button
+      //     // show the answer
+      //   }
+      // }
     }
     // found = true;
   }
@@ -157,203 +194,253 @@ function randomState() {
   var states = [
     {
       name: "Alabama",
-      stateMoto: "We dare defend our rights"
+      stateMoto: "We dare defend our rights",
+      flag: "150px-Flag_of_Alabama.svg.png"
     },
     {
       name: "Alaska",
-      stateMoto: "North to the future"
+      stateMoto: "North to the future",
+      flag: "143px-Flag_of_Alaska.svg.png"
     },
     {
       name: "Arizona",
-      stateMoto: "God enriches"
+      stateMoto: "God enriches",
+      flag: "150px-Flag_of_Arizona.svg.png"
     },
     {
       name: "Arkansas",
-      stateMoto: "The people rule"
+      stateMoto: "The people rule",
+      flag: "150px-Flag_of_Arkansas.svg.png"
     },
     {
       name: "California",
-      stateMoto: "Eureka"
+      stateMoto: "Eureka",
+      flag: "150px-Flag_of_California.svg.png"
     },
     {
       name: "Colorado",
-      stateMoto: "Nothing without the Deity"
+      stateMoto: "Nothing without the Deity",
+      flag: "150px-Flag_of_Colorado_designed_by_Andrew_Carlisle_Carson.svg.png"
     },
     {
       name: "Connecticut",
-      stateMoto: "He who transplanted sustains"
+      stateMoto: "He who transplanted sustains",
+      flag: "130px-Flag_of_Connecticut.svg.png"
     },
     {
       name: "Delaware",
-      stateMoto: "Liberty and Independence"
+      stateMoto: "Liberty and Independence",
+      flag: "150px-Flag_of_Delaware.svg.png"
     },
     {
       name: "Florida",
-      stateMoto: "In God We Trust"
+      stateMoto: "In God We Trust",
+      flag: "150px-Flag_of_Florida.svg.png"
     },
     {
       name: "Georgia",
-      stateMoto: "Wisdom, justice, and moderation"
+      stateMoto: "Wisdom, justice, and moderation",
+      flag: "160px-Flag_of_Georgia_(U.S._state).svg.png"
     },
     {
       name: "Hawaii",
-      stateMoto: "The life of the land is perpetuated in righteousness"
+      stateMoto: "The life of the land is perpetuated in righteousness",
+      flag: "200px-Flag_of_Hawaii.svg.png"
     },
     {
       name: "Idaho",
-      stateMoto: "Let it be perpetual"
+      stateMoto: "Let it be perpetual",
+      flag: "127px-Flag_of_Idaho.svg.png"
     },
     {
       name: "Illinois",
-      stateMoto: "State sovereignty, national union	"
+      stateMoto: "State sovereignty, national union	",
+      flag: "167px-Flag_of_Illinois.svg.png"
     },
     {
       name: "Indiana",
-      stateMoto: "The Crossroads of America	"
+      stateMoto: "The Crossroads of America	",
+      flag: "150px-Flag_of_Indiana.svg.png"
     },
     {
       name: "Iowa",
-      stateMoto: "Our liberties we prize and our rights we will maintain"
+      stateMoto: "Our liberties we prize and our rights we will maintain",
+      flag: "146px-Flag_of_Iowa.svg.png"
     },
     {
       name: "Kansas",
-      stateMoto: "To the stars through difficulties"
+      stateMoto: "To the stars through difficulties",
+      flag: "167px-Flag_of_Kansas.svg.png"
     },
     {
       name: "Kentucky",
-      stateMoto: "United we stand, divided we fall"
+      stateMoto: "United we stand, divided we fall",
+      flag: "190px-Flag_of_Kentucky.svg.png"
     },
     {
       name: "Louisiana",
-      stateMoto: "Union, justice, confidence	"
+      stateMoto: "Union, justice, confidence	",
+      flag: "157px-Flag_of_Louisiana.svg.png"
     },
     {
       name: "Maine",
-      stateMoto: "I lead"
+      stateMoto: "I lead",
+      flag: "150px-Flag_of_Maine.svg.png"
     },
     {
       name: "Maryland",
-      stateMoto: "Manly deeds, womanly words"
+      stateMoto: "Manly deeds, womanly words",
+      flag: "150px-Flag_of_Maryland.svg.png"
     },
     {
       name: "Massachusetts",
-      stateMoto: "By the sword we seek peace, but peace only under liberty"
+      stateMoto: "By the sword we seek peace, but peace only under liberty",
+      flag: "167px-Flag_of_Massachusetts.svg.png"
     },
     {
       name: "Michigan",
-      stateMoto: "If you seek a pleasant peninsula, look about you"
+      stateMoto: "If you seek a pleasant peninsula, look about you",
+      flag: "150px-Flag_of_Michigan.svg.png"
     },
     {
       name: "Minnesota",
-      stateMoto: "The star of the North"
+      stateMoto: "The star of the North",
+      flag: "158px-Flag_of_Minnesota.svg.png"
     },
     {
       name: "Mississippi",
-      stateMoto: "By valor and arms"
+      stateMoto: "By valor and arms",
+      flag: "150px-Flag_of_Mississippi.svg.png"
     },
     {
       name: "Missouri",
-      stateMoto: "Let the welfare of the people be the supreme law"
+      stateMoto: "Let the welfare of the people be the supreme law",
+      flag: "172px-Flag_of_Missouri.svg.png"
     },
     {
       name: "Montana",
-      stateMoto: "Gold and silver"
+      stateMoto: "Gold and silver",
+      flag: "150px-Flag_of_Montana.svg.png"
     },
     {
       name: "Nebraska",
-      stateMoto: "Equality before the law"
+      stateMoto: "Equality before the law",
+      flag: "167px-Flag_of_Nebraska.svg.png"
     },
     {
       name: "Nevada",
-      stateMoto: "All For Our Country and Battle Born"
+      stateMoto: "All For Our Country and Battle Born",
+      flag: "Flag_of_Nevada.svg.png"
     },
     {
       name: "New Hampshire",
-      stateMoto: "Live Free or Die"
+      stateMoto: "Live Free or Die",
+      flag: "150px-Flag_of_New_Hampshire.svg.png"
     },
     {
       name: "New Jersey",
-      stateMoto: "Liberty and prosperity"
+      stateMoto: "Liberty and prosperity",
+      flag: "167px-Flag_of_New_Jersey.svg.png"
     },
     {
       name: "New Mexico",
-      stateMoto: "It grows as it goes"
+      stateMoto: "It grows as it goes",
+      flag: "150px-Flag_of_New_Mexico.svg.png"
     },
     {
       name: "New York",
-      stateMoto: "Ever upward"
+      stateMoto: "Ever upward",
+      flag: "200px-Flag_of_New_York.svg.png"
     },
     {
       name: "North Carolina",
-      stateMoto: "To be, rather than to seem"
+      stateMoto: "To be, rather than to seem",
+      flag: "Flag_of_North_Carolina.svg.png"
     },
     {
       name: "North Dakota",
-      stateMoto: "Liberty and union, now and forever, one and inseparable"
+      stateMoto: "Liberty and union, now and forever, one and inseparable",
+      flag: "128px-Flag_of_North_Dakota.svg.png"
     },
     {
       name: "Ohio",
-      stateMoto: "With God, all things are possible"
+      stateMoto: "With God, all things are possible",
+      flag: "163px-Flag_of_Ohio.svg.png"
     },
     {
       name: "Oklahoma",
-      stateMoto: "Labor conquers all things"
+      stateMoto: "Labor conquers all things",
+      flag: "150px-Flag_of_Oklahoma.svg.png"
     },
     {
       name: "Oregon",
-      stateMoto: "She flies with her own wings"
+      stateMoto: "She flies with her own wings",
+      flag: "167px-Flag_of_Oregon_(reverse).svg.png"
     },
     {
       name: "Pennsylvania",
-      stateMoto: "Virtue, Liberty, and Independence"
+      stateMoto: "Virtue, Liberty, and Independence",
+      flag: "150px-Flag_of_Pennsylvania.svg.png"
     },
     {
       name: "Rhode Island",
-      stateMoto: "Hope"
+      stateMoto: "Hope",
+      flag: "109px-Flag_of_Rhode_Island.svg.png"
     },
     {
       name: "South Carolina",
-      stateMoto: "While I breathe, I hope Ready in soul and resource"
+      stateMoto: "While I breathe, I hope Ready in soul and resource",
+      flag: "150px-Flag_of_South_Carolina.svg.png"
     },
     {
       name: "South Dakota",
-      stateMoto: "Under God the people rule"
+      stateMoto: "Under God the people rule",
+      flag: "Flag_of_South_Dakota.svg.png"
     },
     {
       name: "Tennessee",
-      stateMoto: "Agriculture and Commerce"
+      stateMoto: "Agriculture and Commerce",
+      flag: "167px-Flag_of_Tennessee.svg.png"
     },
     {
       name: "Texas",
-      stateMoto: "Friendship"
+      stateMoto: "Friendship",
+      flag: "150px-Flag_of_Texas.svg.png"
     },
     {
       name: "Utah",
-      stateMoto: "Industry"
+      stateMoto: "Industry",
+      flag: "167px-Flag_of_Utah_(2011-present).svg.png"
     },
     {
       name: "Vermont",
-      stateMoto: "Freedom and Unity"
+      stateMoto: "Freedom and Unity",
+      flag: "167px-Flag_of_Vermont.svg.png"
     },
     {
       name: "Virginia",
-      stateMoto: "Thus always to tyrants"
+      stateMoto: "Thus always to tyrants",
+      flag: "146px-Flag_of_Virginia.svg.png"
     },
     {
       name: "Washington",
-      stateMoto: "By and by"
+      stateMoto: "By and by",
+      flag: "168px-Flag_of_Washington.svg.png"
     },
     {
       name: "West Virginia",
-      stateMoto: "Mountaineers are always free"
+      stateMoto: "Mountaineers are always free",
+      flag: "190px-Flag_of_West_Virginia.svg.png"
     },
     {
       name: "Wisconsin",
-      stateMoto: "Forward"
+      stateMoto: "Forward",
+      flag: "150px-Flag_of_Wisconsin.svg.png"
     },
     {
       name: "Wyoming",
-      stateMoto: "Equal Rights"
+      stateMoto: "Equal Rights",
+      flag: "143px-Flag_of_Wyoming.svg.png"
     }
   ];
 
